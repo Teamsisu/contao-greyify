8ee1e06f2280389385bbe3e8ae5f779f55f7a899
Martin Treml <github@r2pi.net>
2015-01-16 09:41:46 +0100

    fixed not working height/width resize

---
+++ src/Teamsisu/Greyify/Helper/InsertTags.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Greyify
+ * The contao image to greyscale converter
+ *
+ * PHP version 5
+ *
+ * @package    Greyify
+ * @author     Martin Treml <martin.treml@teamsisu.at>
+ * @copyright  Team sisu GmbH
+ * @license    LGPL.
+ */
+
+
+namespace Teamsisu\Greyify\Helper;
+
+use Teamsisu\Greyify\Provider\Greyify;
+
+class InsertTags extends \Controller
+{
+
+    /**
+     * Parse the given Inserttag
+     *
+     * @param string $strTag
+     * @return bool|string - The image in grey
+     */
+    public function parseInsertTags($strTag)
+    {
+
+        $data = explode('::', $strTag);
+
+        switch ($data[0]) {
+
+            case 'greyify':
+
+                if(count($data) > 2){
+
+                    if(!$data[2]){
+                        return false;
+                    }
+
+                    switch(strtolower($data[1]))
+                    {
+
+                        case 'src':
+                            return Greyify::convert($data[2]);
+                            break;
+
+                        case 'tag':
+                            return Greyify::getHTML($data[2], false);
+                            break;
+
+                        case 'figure':
+                            return Greyify::getHTML($data[2], true);
+                            break;
+                    }
+
+                }else{
+                    if(!$data[1]){
+                        return false;
+                    }
+
+                    return Greyify::getHTML($data[1], true);
+                }
+
+                break;
+        }
+
+        return false;
+
+    }
+
+} 
